5919612cadec2d0d228b52037fba56ac29c53414
Yashpa12 <yashbhamare1004@gmail.com>
2024-07-14 06:36:05 +0530

    creating navbar and foooter

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const path = require("path");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/listings");
-
+const methodOverride = require("method-override");
+const ejsmate = require('ejs-mate')
 mongoose
   .connect(MONGO_URL)
   .then(() => {
@@ -13,10 +14,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+app.engine('ejs',ejsmate)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.get("/listings", async (req, res) => {
   const allList = await Listing.find({});
@@ -53,6 +58,41 @@ app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const showDetails = await Listing.findById(id);
   res.render("listings/show", { showDetails });
+});
+
+// edit
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const showDetails = await Listing.findById(id);
+  res.render("listings/edit", { showDetails });
+});
+
+// update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let { title, description, image, price, location, country } = req.body;
+  try {
+    await Listing.findByIdAndUpdate(id, {
+      title,
+      description,
+      image,
+      price,
+      location,
+      country,
+    });
+    res.redirect(`/listings/${id}`);
+  } catch (err) {
+    console.error("Error updating listing:", err);
+    res.send("Error updating listing");
+  }
+});
+
+// delete routes
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deleteList = await Listing.findByIdAndDelete(id);
+  console.log(deleteList);
+  res.redirect("/listings");
 });
 
 app.get("/", (req, res) => {
